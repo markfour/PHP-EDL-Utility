f1d73cd363c450e207c4587a7632c7d609e45356
markfour <axhfaro@gmail.com>
2021-05-17 00:11:41 +0900

    Initialize

---
+++ edl-utility.php
@@ -0,0 +1,50 @@
+<?php
+	// TODO ソースのクラスを書く
+	final class Source {
+		public $number = '';
+	}
+
+	final class EDL {
+	    // プロパティの宣言
+	    public $title = '';
+	    public $fcm = '';
+	    public $sources = [];
+
+	    // メソッドの宣言
+	    public function displayVar() {
+	        echo $this->title;
+	    }
+	}
+
+	$edl = new EDL();
+	$edl->$sources = []; // クラス内に配列配列に代入がうまくいかない?
+	$sources = [];
+
+	// ファイルを変数に格納
+	$filename = 'test-edl.txt';
+
+	// ファイルを配列に格納し、さらに変数に格納
+	$lines = file($filename);
+
+	// 配列を出力
+	// print_r($lines);
+
+	foreach($lines as $line){
+		if (preg_match('/^TITLE:/',$line, $matches)) {
+			$edl->$title = str_replace('TITLE: ', '', $line);
+
+		} else if (preg_match('/^FCM:/',$line, $matches)) {
+			$edl->$fcm = str_replace('FCM: ', '', $line);
+
+		} else if (preg_match('/^\d{6} /',$line, $matches)) {
+			array_push($sources, $matches[0]);
+		}
+	}
+
+	// TODO このIF文にファイル解析を作る
+	print('Title:'.$edl->$title);
+	print('FCM:'.$edl->$fcm);
+	foreach ($sources as $v) {
+		print('sources:'.$v);
+	}
+?>
